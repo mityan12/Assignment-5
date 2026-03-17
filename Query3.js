7f690ab6c464382208cd72ce294ceed814f58df6
mityan12 <mit78509@gmail.com>
2026-03-16 20:58:35 -0700

    Add files via upload

---
+++ Query3.js
@@ -0,0 +1,38 @@
+const { MongoClient } = require("mongodb");
+
+async function runQuery() {
+    const client = new MongoClient("mongodb://127.0.0.1:27017");
+
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+
+        const db = client.db("ieeevisTweets");
+        const collection = db.collection("tweet");
+
+        const result = await collection.aggregate([
+            {
+                $group: {
+                    _id: "$user.screen_name",
+                    tweetCount: { $sum: 1 }
+                }
+            },
+            {
+                $sort: { tweetCount: -1 }
+            },
+            {
+                $limit: 1
+            }
+        ]).toArray();
+
+        console.log("User with most tweets:");
+        console.log(result);
+
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await client.close();
+    }
+}
+
+runQuery();
